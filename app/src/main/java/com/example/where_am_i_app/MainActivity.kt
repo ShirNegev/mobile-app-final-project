3286276ecf28d879c4ba350c4c2fada56de14a99
ShirNegev <shirbdc@gmail.com>
2025-03-23 18:38:00 +0200

    feat(bottom-navigation): Added bottom navigation bar that navigate between the fragments

--- app/src/main/java/com/example/where_am_i_app/MainActivity.kt
+++ app/src/main/java/com/example/where_am_i_app/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.where_am_i_app
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -25,5 +27,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val navHostController: NavHostFragment? = supportFragmentManager.findFragmentById(R.id.main_nav_host) as? NavHostFragment
+        navController = navHostController?.navController
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_bar)
+        navController?.let { NavigationUI.setupWithNavController(bottomNavigationView, it) }
     }
 }
